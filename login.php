6bef9027261663b4939fbd841ca978ad8a6d09f8
xferran <s1174937@student.windesheim.nl>
2021-11-30 10:20:17 +0100

    versie 0.3.8

--- login.php
+++ login.php
@@ -1,16 +1,24 @@
 <?php
 include __DIR__ . "/header.php";
-include __DIR__ . "/functions.php"
+include __DIR__ . "/functions.php";
+include __DIR__ . "/footer.php";
 ?>
 
 <form
-    <div class="container">
-        <br><br><br><br><br><br>
-        <label for="uname"><b>Gebruikersnaam</b></label>
+    <div class="login">
+        <br> <br>  <h1><center>Inloggen:</center></h1>
+        <br>
+        <label for="uname"> <b>Gebruikersnaam:</b></label>
         <input type="text" placeholder="Vul hier uw gebruikersnaam in" name="uname" required>
 
-        <label for="psw"><b>Wachtwoord</b></label>
-        <input type="password" placeholder="Vul hier uw wachtwoord in" name="psw" required>
+        <br><br> <label  for="psw"><b>Wachtwoord:</b></label>
+        <input type="password" placeholder="Vul hier uw wachtwoord in" name="psw" required >
+        <br> <br>
+        <br>
+        <input type="submit" value="Inloggen" style="background-color: #e1e1e1"
+        <br> <br><br>
+        <input type="checkbox"  id="remember" value="Herinner mij" >
+        <label for="remember"> Herinner mij</label>
 
 
 
