5b086460fcd96cd34a0e0b3c347883cd18147482
xferran <s1174937@student.windesheim.nl>
2021-12-01 14:55:39 +0100

    versie 0.4

--- login.php
+++ login.php
@@ -35,6 +35,6 @@ include __DIR__ . "/footer.php";
 <div>
     <br> <a href="wachtwoord%20vergeten.php" style="margin: 1%" >Wachtwoord vergeten?</a>
 </div>
-
-
+<br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 </form>
