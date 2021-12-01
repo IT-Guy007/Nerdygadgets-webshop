acebbb5eef53a566c26c656769db79319a4fb60e
Thijs Onderwater <thijs.onderwater2000@gmail.com>
2021-12-01 02:05:22 +0100

    4.0.3

--- login.php
+++ login.php
@@ -11,21 +11,22 @@ include __DIR__ . "/footer.php";
     <b>Bestaande klant</b>
     <br>
 
-    <input type="text" placeholder="Vul hier uw gebruikersnaam in" name="uname" required style="width: 30%">
+    <input type="text" placeholder="Vul hier uw gebruikersnaam in" name="uname" required style="width: 25%">
     <br>
     <label for="psw"><b></b></label>
     <br>
-    <input type="password" placeholder="Vul hier uw wachtwoord in" name="psw" required style="width: 30%">
+    <input type="password" placeholder="Vul hier uw wachtwoord in" name="psw" required style="width: 25%">
     <br> <br>
 </div>
 <div class="inloggen" style="display: flex;">
-    <input type="submit" value="Inloggen" style="width:20%;background-color:#e1e1e1;">
+
+    <input class="buttonInlog buttonInlog1" type="submit" value="Inloggen" style="width:20%;">
     <input type="checkbox" id="remember" value="Herinner mij" style="width: 40px; margin-left: 2%">
     <label for="remember" style="margin: 1%">Gebruikersnaam opslaan</label>
     <br>
 </div>
 <div>
-    <a href="login.php">Wachtwoord vergeten?</a>
+    <a href="login.php" style="margin: 1%;margin-top: 1%">Wachtwoord vergeten?</a>
 </div>
 
 
