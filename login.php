f808f009e7ca5f5b279a726f2fc0cd4609182e6d
xferran <s1174937@student.windesheim.nl>
2021-12-01 10:04:50 +0100

    versie 0.3.9

--- login.php
+++ login.php
@@ -9,24 +9,30 @@ include __DIR__ . "/footer.php";
     <br> <br>
     <h1>Inloggen</h1>
     <b>Bestaande klant</b>
+    <b STYLE="position: relative; left:300px;"> Nog geen account?</b>
     <br>
 
-    <input type="text" placeholder="Gebruikersnaam" name="uname" required style="width: 30%">
+    <input type="text" placeholder="Gebruikersnaam" name="uname" required style="position: relative; top: 10px; width: 40%">
+
+    <input type="text" placeholder="E-mailadres" name="uname"  style="position: relative; left:90px; top: 10px; width: 40%">
     <br>
     <label for="psw"><b></b></label>
     <br>
-    <input type="password" placeholder="Wachtwoord" name="psw" required style="width: 30%">
+    <input type="password" placeholder="Wachtwoord" name="psw" required style="width: 40%">
+    <input style="width:20%; position: relative; left 300px;" class="buttonInlog buttonInlog1" type="button" value="Account aanmaken">
+
     <br> <br>
 </div>
 <div class="inloggen" style="display: flex;">
 
-    <input class="buttonInlog buttonInlog1" type="submit" value="Inloggen" style="width:20%;">
-    <input type="checkbox" id="remember" value="Herinner mij" style="width: 40px; margin-left: 2%">
+    <input class="buttonInlog buttonInlog1" type="submit" value="Inloggen" style="width:15%;">
+    <input type="checkbox" id="remember" value="Herinner mij" style="width: 25px; margin-left: 2%">
     <label for="remember" style="margin: 1%">Gebruikersnaam opslaan</label>
-    <br>
+
+
 </div>
 <div>
-    <a href="password-request.php" style="margin: 1%;margin-top: 1%">Wachtwoord vergeten?</a>
+    <br> <a href="wachtwoord%20vergeten.php" style="position: left: 10px;margin-top: 1%">Wachtwoord vergeten?</a>
 </div>
 
 
