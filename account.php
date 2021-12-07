8424d3c99056040ebe9839dc6601210b6839a750
Jeroen <76699269+IT-Guy007@users.noreply.github.com>
2021-12-07 16:11:22 +0100

    V0.4.7.1 Begin account
    Signed-off-by: Jeroen <76699269+IT-Guy007@users.noreply.github.com>

--- account.php
+++ account.php
@@ -1,3 +1,28 @@
 <?php
-include __DIR__ . "/header.php";
+//In this file will be used for logging in, registering and order summary.
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+if(isset($_SESSION['email'])) {
+    $email = $_SESSION['email'];
+}
+
+if(isset($_SESSION['password'])) {
+    $password = $_SESSION['password'];
+}
+
+/*
+if ($_SESSION['loggedin']) {
+    header("Location: login.php");
+
+} else {
+    include __DIR__ . "/header.php";
+    ?>
+    <p1>Account pagina</p1>
+<?php
+}
+*/
 ?>
+
+
