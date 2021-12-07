ba9f0eddd448b5ac36d99722ec1c6dc745a84f08
Jeroen <76699269+IT-Guy007@users.noreply.github.com>
2021-12-07 11:59:32 +0100

    V0.4.7 Cart is af
    Signed-off-by: Jeroen <76699269+IT-Guy007@users.noreply.github.com>

--- cart.php
+++ cart.php
@@ -1,57 +1,66 @@
 <?php
-include __DIR__ . "/cartfuncties.php";
 include __DIR__ . "/header.php";
+include __DIR__ . "/cartfuncties.php";
 $br = $total = 0;
 $cart = getCart();
 ?>
 <?php
 if (empty($cart)) { ?>
-<br>
-<div class="CartContainer">
-    <div class="Header">
+    <br>
+    <div class="CartContainer">
+        <div class="Header">
+            <br>
+            <h3 class="Heading" >Winkelwagen is leeg</h3>
+        </div>
         <br>
-        <h3 class="Heading" >Winkelwagen is leeg</h3>
+        <br>
+        <br>
     </div>
     <br>
     <br>
     <br>
-</div>
-    <br>
-    <br>
-    <br>
-    <?php } else { ?>
+<?php } else { ?>
     <div class="CartContainer">
         <div class="Header">
             <h3 class="Heading" style="margin-top: 10px">Winkelwagen</h3>
-            <h3 class="Action" href="/cartfuncties.php" >Verwijder alle items</h3>
+            <form action="cartfuncties.php" target="_self">
+                <input type="submit" id="cartitem" class="remove" name="emptycart" value="Verwijder alle items">
+            </form>
         </div>
         <!--Voor elke item-->
         <?php
+        $amountarikels = 0;
         foreach($cart as $item => $amount):
             $itemarray = (getItemDetails($item,$databaseConnection));
             $total = $total + ($itemarray["SellPrice"] * $amount);
+            $amountarikels = $amountarikels + $amount;
             ?>
             <div class="Cart-Items">
-              <div class="image-box">
-                <img src = "<?php if (!empty($itemarray["BackupImagePath"])) {
-                    print("/nerdygadgets/public/stockgroupimg/" . $itemarray["BackupImagePath"]);
-                }  else { print("/nerdygadgets/public/img/nologo.png");} ?>" style ="height: 120px; margin: 6%" />
+                <div class="image-box">
+                    <img src = "<?php if (!empty($itemarray["BackupImagePath"])) {
+                        print("/nerdygadgets/public/stockgroupimg/" . $itemarray["BackupImagePath"]);
+                    }  else { print("/nerdygadgets/public/img/nologo.png");} ?>" style ="height: 120px; margin: 6%" />
+                </div>
+                <div class="about" >
+                    <b class="title" > <?php echo $itemarray["StockItemName"]?> </b>
+                    <h3 class="subtitle" > Artikelnummer: <?php echo $itemarray["StockItemID"]?></h3>
+                </div>
+                <form action="cartfuncties.php" target="_self">
+                    <input type="submit" class="btn" id="cartitem" name="quantitymin" value="-">
+                    <div class="count" > <?php print($amount)?></div>
+                    <input type="submit" class="btn" id="cartitem" name="quantityplus" value="+">
+                    <input type="hidden" class="btn" id="cartitem" name="cartitemid" value="<?php print($itemarray["StockItemID"])?>">
+                    <input type="hidden" class="btn" id="cartitem" name="amount" value="<?php print($amount)?>">
+                </form>
+                <div class="prices"  >
+                    <form action="cartfuncties.php" target="_self">
+                        <div class="amount" > <?php print($itemarray["SellPrice"] * $amount)?></div>
+                        <input type="hidden" id="cartitem" name="removecartitemid" value="<?php print($itemarray["StockItemID"])?>">
+                        <input type="submit" id="cartitem" class="remove" name="removecartitem" value="Verwijder">
+                    </form>
+                </div>
             </div>
-            <div class="about" >
-                <b class="title" > <?php echo $itemarray["StockItemName"]?> </b>
-                <h3 class="subtitle" > Artikelnummer: <?php echo $itemarray["StockItemID"]?></h3>
-            </div>
-            <div class="counter">
-                <div class="btn" > - </div>
-                <div class="count" > <?php print($amount)?></div>
-                <div class="btn" > +</div>
-            </div>
-            <div class="prices" >
-                <div class="amount" > <?php print($itemarray["SellPrice"] * $amount)?></div>
-                <div class="remove" ><u>Verwijder</u></div>
-            </div>
-        </div>
-        <br>
+            <br>
         <?php endforeach; ?>
         <hr>
         <!--Totaal-->
@@ -59,14 +68,13 @@ if (empty($cart)) { ?>
             <div class="total">
                 <div>
                     <div class="Subtotal">Totaal</div>
-                    <div class="items">Aantal: <?php print(count($cart))?></div>
+                    <div class="items">Aantal: <?php print($amountarikels)?></div>
                 </div>
                 <div class="total-amount"><?php print("€ " . $total)?></div>
             </div>
             <button href="checkout.php" class="button">Afrekenen</button></div>
     </div>
     <?php
-    $br = 0;
     while($br < 8) {
         print("<br>");
         $br++;
