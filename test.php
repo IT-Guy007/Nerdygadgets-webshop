fff549938870f49e8e78fbad1da8053a9b1f7630
Jeroen <76699269+IT-Guy007@users.noreply.github.com>
2021-12-17 13:18:24 +0100

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-include __DIR__ . "/header.php";
-print("---------------");
-$categories = (getAllCustomerCategories($databaseConnection));
-print_r($categories);
-$countries = getAllCountries($databaseConnection);
-print_r($countries);
