87f1df7909d35564f71e072908c5d1f812039876
Thijs Onderwater <thijs.onderwater2000@gmail.com>
2021-12-21 15:49:34 +0100

    0.5.4 ratings in php

--- ratings2.php
+++ ratings2.php
@@ -8,12 +8,7 @@
     ?>
     <meta charset="UTF-8">
     <title>Rating system 2</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        form {
-            width: 120px;
-        }
-    </style>
+
 </head>
 <body>
 
@@ -21,17 +16,17 @@
 $loggedin = true;
 if ($loggedin) { ?>
     <div style="background: rgb(36, 41, 54); padding: 50px">
-        <form action="" method="POST">
+        <form class="ratingForm" action="" method="POST">
             <button class="buttonStar" type="submit" name="rating[rating]" value="5">★</button>
             <button class="buttonStar" type="submit" name="rating[rating]" value="4">★</button>
             <button class="buttonStar" type="submit" name="rating[rating]" value="3">★</button>
             <button class="buttonStar" type="submit" name="rating[rating]" value="2">★</button>
             <button class="buttonStar" type="submit" name="rating[rating]" value="1">★</button>
         </form>
-
-
     </div>
-<?php } ?>
+<?php }
+
+?>
 
 
 </body>
